aaf3e093b4e9b47895fb39ca6a777582a4ebba66
JuanDiego00 <00.juandiegogomez@gmail.com>
2026-09-07 11:15:22 -0500

    implementacion de bycriptjs en un entoro de prueba

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -3,7 +3,7 @@ import{ crearUsuario, verificarUsuario } from "../controllers/Users.js"
 
 const router = Router();
 
-router.post("/register", crearUsuario);
-router.post("/login", verificarUsuario);
+router.post('/register', crearUsuario);
+router.post('/login', verificarUsuario);
 
 export default router;
